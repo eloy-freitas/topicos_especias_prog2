5af45d082a37c2228663eb7b9d4ea060a995beec
eloy-freitas <freitaseloy.1@gmail.com>
2022-09-21 00:28:54 -0300

    feat: build dir

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir $1
+mkdir $1/inputs
+
+for i in 1 2 3; do
+    touch $1/inputs/i0$i.txt
+done
+
+touch $1/main.cpp
+
+echo "#include <iostream>" > $1/main.cpp
+echo "int main(int argc, char *argv[]){" >> $1/main.cpp
+echo "}" >> $1/main.cpp
+
+cp Makefile $1/

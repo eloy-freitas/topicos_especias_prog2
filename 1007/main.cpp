a46e258e010e003bbd8a991fab5280a7fddf6d14
eloy-freitas <freitaseloy.1@gmail.com>
2022-09-21 00:41:14 -0300

    feat: 1007

---
+++ 1007/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(int argc, char *argv[]){
+    int A, B, C, D, DIFERENCA = 0;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+    DIFERENCA = (A * B - C *D);
+
+    printf("DIFERENCA = %d\n", DIFERENCA);
+
+}

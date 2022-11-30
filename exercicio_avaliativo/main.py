885a26338e8ed3618cf6c5eb82c2c9025dab9b08
eloy-freitas <freitaseloy.1@gmail.com>
2022-11-29 23:45:00 -0300

    fix: refinando código

--- exercicio_avaliativo/main.py
+++ exercicio_avaliativo/main.py
@@ -8,14 +8,14 @@ def create_matrix(rowCount):
 
 
 if __name__ == '__main__':
-    fila = []
-    
     while(True):
         size = int(input())
+        fila = []
         if size != 0:
+            # monta matrix de adjacência
             alunos = create_matrix(size)
             cores = [-1 for x in range(size)]
-            for x in range(size):
+            for _ in range(size):
                 i = int(input()) - 1
                 amigos = [int(x) - 1 for x in input().split(" ")]
                 for j in amigos:
@@ -25,17 +25,23 @@ if __name__ == '__main__':
             cor_atual = 0
 
             # busca em largura
-            while len(fila) != 0:
+            while -1 in cores:
                 i = fila.pop()
                 for j in range(size):
                     if alunos[i][j] == 1 and cores[j] == -1:
                         cores[j] = cor_atual
                         fila.append(j)
+                    
                 cor_atual = 1 if cor_atual == 0 else 0
+                if len(fila) == 0:
+                    try:
+                        fila.append(cores.index(-1))
+                    except:
+                        break
                     
             isBipartido = True
-            
-            #verifica se o grafo é bipartido
+ 
+            # verifica se o grafo é bipartido
             for i in range(size):
                 for j in range(size):
                     if alunos[i][j] == 1 and cores[j] == cores[i]:
